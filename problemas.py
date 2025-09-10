16c7121724640b81f1e76dac6932fd12d081ed69
Mag <migalindor@unal.edu.co>
2025-09-10 12:42:50 -0500

    Agregue el segundo ejercicio

--- problemas.py
+++ problemas.py
@@ -2,3 +2,10 @@
 palabra = "esto es una oracion"
 conteo = palabra.count("e")
 print (conteo)
+#Elabore un programa que dada una cadena diga si todos los sımbolos de la cadena son letras.
+var= str(input("Ingresa lo que quieras con o sin espacios, seas numeros o letras: "))
+Result= var.isalpha()
+if (Result==True):
+    print("Si funciona")
+else:
+    print("No funciona")
